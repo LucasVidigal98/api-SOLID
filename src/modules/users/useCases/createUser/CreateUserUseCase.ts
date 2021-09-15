87b815da52b4b3c104e24c7ff88b0a0d7fac2caf
LucasVidigal98 <lucasvidigal3@gmail.com>
2021-09-15 20:31:27 -0300

    Completed the code

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,15 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const searchUser = this.usersRepository.findByEmail(email);
+
+    if (searchUser) {
+      throw new Error();
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
   }
 }
 
